5176a6f69d43a6ccb52e365e910ce9e3bcc35972
David-Inkheart <okoliedave1@gmail.com>
2023-05-04 14:07:11 +0000

    corrected error in docs

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,13 +5,14 @@
 -- LAST_INSERT_ID() function.
 -- Finally, the procedure inserts a new correction into the corrections table,
 -- with the given user_id, the project_id of the project
---- (either retrieved or inserted in the previous step), and the given score.
+-- (either retrieved or inserted in the previous step), and the given score.
 
 -- To call this procedure, you can simply use the following SQL statement:
 -- CALL AddBonus(user_id, project_name, score);
-
 DELIMITER $$
+
 DROP PROCEDURE IF EXISTS AddBonus;
+
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
     DECLARE project_id INT;
