f7157b0be03e95984f9801a8617b41c68ffaecc3
David-Inkheart <okoliedave1@gmail.com>
2023-05-04 10:29:32 +0000

    all bands with Glam rock as their main style

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- lists all bands with Glam rock as their main style
 -- ranked by their longevity
-SELECT band_name, (COALESCE(split, YEAR(NOW())) - formed) AS lifespan
+SELECT band_name, (COALESCE(split, 2022)) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
