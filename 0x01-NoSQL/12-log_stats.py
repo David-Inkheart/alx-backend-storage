a7129f666fc5c7c6453c98015665535948fd5f17
David-Inkheart <okoliedave1@gmail.com>
2023-05-09 18:54:05 +0100

    some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -18,13 +18,3 @@ if __name__ == "__main__":
     filter_path = {"method": "GET", "path": "/status"}
     num_path = logs_collection.count_documents(filter_path)
     print("{} status check".format(num_path))
-
-    # print("IPs:")
-    # pipeline = [
-    #     {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-    #     {"$sort": {"count": -1}},
-    #     {"$limit": 10}
-    # ]
-    # ips = logs_collection.aggregate(pipeline)
-    # for ip in ips:
-    #     print("\t{}: {}".format(ip.get("_id"), ip.get("count")))
